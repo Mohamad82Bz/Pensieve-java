ba4bbb008270f308b6d0c0434c68bfdb658d3e92
Mohamad82 <mohamad.hossein82.bz@gmail.com>
2024-07-23 14:49:02 +0330

    add PensieveReplayerAddEntityEvent

---
+++ src/main/java/me/mohamad82/pensieve/api/event/PensieveReplayerAddEntityEvent.java
@@ -0,0 +1,55 @@
+package me.mohamad82.pensieve.api.event;
+
+import me.mohamad82.pensieve.recording.record.EntityRecord;
+import me.mohamad82.pensieve.replaying.PlayBackControl;
+import me.mohamad82.pensieve.replaying.Replayer;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class PensieveReplayerAddEntityEvent extends Event implements Cancellable {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    private boolean cancelled;
+
+    private final Replayer replayer;
+    private final PlayBackControl playBackControl;
+    private final int tickIndex;
+
+    public PensieveReplayerAddEntityEvent(Replayer replayer, PlayBackControl playBackControl, int tickIndex, EntityRecord record) {
+        this.replayer = replayer;
+        this.playBackControl = playBackControl;
+        this.tickIndex = tickIndex;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public Replayer getReplayer() {
+        return replayer;
+    }
+
+    public PlayBackControl getPlayBackControl() {
+        return playBackControl;
+    }
+
+    public int getTickIndex() {
+        return tickIndex;
+    }
+}
